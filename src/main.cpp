53446f4f0f3021068edf0d55cd0191479baef8fc
lnikon <bejanyan.vahag@protonmail.com>
2020-12-23 22:01:52 +0400

    Connect to ws://echo.websocket.org (sync)

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <boost/asio.hpp>
 #include <boost/system/error_code.hpp>
+#include <boost/assert.hpp>
+#include <boost/beast.hpp>
 
 #include <iomanip>
 #include <iostream>
 #include <thread>
 
+namespace net = boost::asio;
+namespace beast = boost::beast;
+using namespace boost::beast;
+using namespace boost::beast::websocket;
 using tcp = boost::asio::ip::tcp;
 
 void Log(boost::system::error_code ec)
@@ -23,37 +29,64 @@ int main()
     std::cerr << "[" << std::setw(14) << std::this_thread::get_id() << "] main" << std::endl;
 
     boost::asio::io_context ioc;
-
-    tcp::socket socket(boost::asio::make_strand(ioc));
-
-    size_t nThreads{4};
+    tcp::socket socket(ioc);
 
     boost::system::error_code ec;
-    tcp::resolver resolver(ioc);
-    auto address{boost::asio::ip::address::from_string("1.1.1.1")};
-    auto endpoint{resolver.resolve("google.com", "80", ec)};
+
+	tcp::resolver resolver(ioc);
+	const std::string url("echo.websocket.org");
+	const std::string port("80");
+	tcp::resolver::results_type results = resolver.resolve(url, port, ec);
+	BOOST_ASSERT_MSG(results.begin() != results.end(), "empty endpoints for echo.websocket.org:80");
+	auto endpoint{*results.begin()};
     if (ec)
     {
         Log(ec);
         return -1;
     }
 
-    for (size_t idx{0}; idx < nThreads; idx++)
+	socket.connect(endpoint, ec);
+    if (ec)
     {
-        socket.async_connect(*endpoint, OnConnect);
+        Log(ec);
+        return -2;
     }
 
-    std::vector<std::thread> threads;
-    threads.reserve(nThreads);
-    for (size_t idx{0}; idx < nThreads; idx++)
+	stream<tcp_stream> ws(std::move(socket));
+	ws.handshake(url, "/", ec);
+    if (ec)
     {
-        threads.emplace_back([&ioc]() { ioc.run(); });
+        Log(ec);
+        return -3;
     }
 
-	for (size_t idx{0}; idx < nThreads; idx++)
-	{
-		threads[idx].join();
-	}
+	ws.text(true);
+
+	std::string handshakeStr = "GET / HTTP/1.1"
+							"Host: echo.websocket.org"
+							"Upgrade: websocket"
+							"Connection: Upgrade";
+	boost::asio::const_buffer wbuffer(handshakeStr.data(), handshakeStr.size());
+	ws.write(wbuffer, ec);
+    if (ec)
+    {
+        Log(ec);
+        return -3;
+    }
+
+	boost::beast::flat_buffer rbuffer;
+	ws.read(rbuffer, ec);
+
+	std::cout << "ECHO: "
+			<< boost::beast::make_printable(rbuffer.data())
+			<< std::endl;
+
+	// ioc.run(ec);
+    // if (ec)
+    // {
+    //     Log(ec);
+    //     return -3;
+    // }
 
     return 0;
 }
