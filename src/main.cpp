67959214a970b8c3063ff03879c75d3b1350eb44
lnikon <bejanyan.vahag@protonmail.com>
2020-12-23 15:38:20 +0400

    Add boost asio code

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,59 @@
 #include <boost/asio.hpp>
 #include <boost/system/error_code.hpp>
 
+#include <iomanip>
 #include <iostream>
+#include <thread>
+
+using tcp = boost::asio::ip::tcp;
+
+void Log(boost::system::error_code ec)
+{
+    std::cerr << "[" << std::setw(14) << std::this_thread::get_id() << "] "
+              << (ec ? "Error: " : "OK") << (ec ? ec.message() : "") << std::endl;
+}
+
+void OnConnect(boost::system::error_code ec)
+{
+    Log(ec);
+}
 
 int main()
 {
+    std::cerr << "[" << std::setw(14) << std::this_thread::get_id() << "] main" << std::endl;
+
+    boost::asio::io_context ioc;
+
+    tcp::socket socket(boost::asio::make_strand(ioc));
+
+    size_t nThreads{4};
+
     boost::system::error_code ec;
+    tcp::resolver resolver(ioc);
+    auto address{boost::asio::ip::address::from_string("1.1.1.1")};
+    auto endpoint{resolver.resolve("google.com", "80", ec)};
     if (ec)
     {
-        std::cerr << "Error: " << ec.message() << std::endl;
+        Log(ec);
+        return -1;
     }
-    else
+
+    for (size_t idx{0}; idx < nThreads; idx++)
     {
-        std::cout << "OK" << std::endl;
+        socket.async_connect(*endpoint, OnConnect);
     }
+
+    std::vector<std::thread> threads;
+    threads.reserve(nThreads);
+    for (size_t idx{0}; idx < nThreads; idx++)
+    {
+        threads.emplace_back([&ioc]() { ioc.run(); });
+    }
+
+	for (size_t idx{0}; idx < nThreads; idx++)
+	{
+		threads[idx].join();
+	}
 
     return 0;
 }
