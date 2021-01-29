65c9f343a9823711adeac89b1876de3bdfb7879e
lnikon <bejanyan.vahag@protonmail.com>
2021-01-29 13:06:48 +0400

    Add transport network class

--- inc/network-monitor/transport-network.h
+++ inc/network-monitor/transport-network.h
@@ -3,7 +3,28 @@
 
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include <vector>
+
+namespace Utility
+{
+template <typename IdType, typename ObjectType> IdType getId(const ObjectType& object)
+{
+    return object.id;
+}
+
+template <typename IdType, typename ValueType>
+std::unordered_map<IdType, ValueType> vectorToUnorderedMap(const std::vector<ValueType>& vec)
+{
+    std::unordered_map<IdType, ValueType> result;
+    for (const auto& item : vec)
+    {
+        result.emplace(getId(item), item);
+    }
+
+    return result;
+}
+} // namespace Utility
 
 namespace NetworkMonitor
 {
@@ -68,6 +89,51 @@ public:
 
     unsigned int
     GetTravelTime(const Line& line, const Route& route, const Id& stationA, const Id& stationB);
+
+private:
+    struct GraphNode;
+    struct GraphEdge;
+    struct RouteInternal;
+    struct LineInternal;
+
+    struct GraphNode
+    {
+        Id id{};
+        Name name{};
+        long long int passengerCount{0};
+        std::vector<GraphEdge> edges{};
+    };
+
+    struct GraphEdge
+    {
+        std::shared_ptr<RouteInternal> route{nullptr};
+        std::shared_ptr<GraphNode> nextStop{nullptr};
+        unsigned int travelTime{0};
+    };
+
+    struct RouteInternal
+    {
+        Id id{};
+        Name name{};
+        std::shared_ptr<LineInternal> line{nullptr};
+        std::vector<std::shared_ptr<GraphNode>> stops{};
+    };
+
+    struct LineInternal
+    {
+        Id id{};
+        Name name{};
+        std::unordered_map<Id, std::shared_ptr<RouteInternal>> routes{};
+    };
+
+    std::unordered_map<Id, std::shared_ptr<GraphNode>> m_stations{};
+    std::unordered_map<Id, std::shared_ptr<LineInternal>> m_lines{};
+
+    // Helper functions
+    std::shared_ptr<GraphNode> getStation(const Id& id) const;
+    std::shared_ptr<LineInternal> getLine(const Id& id) const;
+
+    bool addRouteToLine(const Route& route, const std::shared_ptr<LineInternal>& lineInternal);
 };
 
 } // namespace NetworkMonitor
--- src/transport-network.cpp
+++ src/transport-network.cpp
@@ -1,5 +1,7 @@
 #include <network-monitor/transport-network.h>
 
+#include <stdexcept>
+
 namespace NetworkMonitor
 {
 
@@ -25,23 +27,99 @@ TransportNetwork& TransportNetwork::operator=(TransportNetwork&& moved)
 
 bool TransportNetwork::AddStation(const Station& station)
 {
-	return false;
+    if (getStation(station.id) != nullptr)
+    {
+        return false;
+    }
+
+    auto node{std::make_shared<GraphNode>(GraphNode{station.id, station.name, 0, {}})};
+    m_stations.emplace(node->id, std::move(node));
+    return true;
 }
 
 bool TransportNetwork::AddLine(const Line& line)
 {
+    if (getLine(line.id) != nullptr)
+    {
+        return false;
+    }
+
+    auto lineInternal{std::make_shared<LineInternal>(LineInternal{line.id, line.name, {}})};
+    for (const auto& route : line.routes)
+    {
+        if (!addRouteToLine(route, lineInternal))
+        {
+            return false;
+        }
+    }
+
+    m_lines.emplace(lineInternal->id, lineInternal);
+
+    return true;
 }
 
 bool TransportNetwork::RecordPassengerEvent(const Id& station, const PassengerEvent& event)
 {
+    const auto node{getStation(station)};
+    if (node == nullptr)
+    {
+        return false;
+    }
+
+    switch (event)
+    {
+    case PassengerEvent::In:
+        node->passengerCount++;
+        return true;
+    case PassengerEvent::Out:
+        node->passengerCount--;
+        return true;
+    default:
+        return false;
+    };
 }
 
 long long int TransportNetwork::GetPassengerCount(const Id& station) const
 {
+    const auto node{getStation(station)};
+    if (nullptr == node)
+    {
+        throw std::runtime_error("Could not find station in the network: " + station);
+    }
+
+    return node->passengerCount;
 }
 
 std::vector<Id> TransportNetwork::GetRoutesServingStation(const Id& station) const
 {
+    const auto node{getStation(station)};
+    if (nullptr == node)
+    {
+        throw std::runtime_error("Could not find station in the network: " + station);
+    }
+
+    const auto& edges {node->edges};
+    std::vector<Id> routes{};
+    routes.reserve(edges.size());
+	const auto size = edges.size();
+	for (const auto& edge : edges)
+	{
+		routes.push_back(edge.route->id);
+	}
+
+    // FIXME: In the worst case, we are iterating over all routes
+    // for all lines in the network. We may want to optimize this.
+    for (const auto& [_, line] : m_lines)
+    {
+        for (const auto& [_, route] : line->routes)
+        {
+            const auto endStop{route->stops.back()};
+            if (endStop == node)
+            {
+                routes.push_back(route->id);
+            }
+        }
+    }
 }
 
 bool TransportNetwork::SetTravelTime(const Id& stationA,
@@ -59,6 +137,57 @@ unsigned int TransportNetwork::GetTravelTime(const Line& line,
                                              const Id& stationA,
                                              const Id& stationB)
 {
+}
+
+std::shared_ptr<TransportNetwork::GraphNode> TransportNetwork::getStation(const Id& id) const
+{
+    auto itStation = m_stations.find(id);
+    return (itStation != m_stations.end() ? itStation->second : nullptr);
+}
+
+std::shared_ptr<TransportNetwork::LineInternal> TransportNetwork::getLine(const Id& id) const
+{
+    auto itLine = m_lines.find(id);
+    return (itLine != m_lines.end() ? itLine->second : nullptr);
+}
+
+bool TransportNetwork::addRouteToLine(
+    const Route& route,
+    const std::shared_ptr<TransportNetwork::LineInternal>& lineInternal)
+{
+    if (lineInternal->routes.find(route.id) != lineInternal->routes.end())
+    {
+        return false;
+    }
+
+    std::vector<std::shared_ptr<GraphNode>> stops{};
+    stops.reserve(route.stops.size());
+    for (const auto& stop : route.stops)
+    {
+        const auto& station{getStation(stop)};
+        if (station == nullptr)
+        {
+            return false;
+        }
+
+        stops.push_back(station);
+    }
+
+    auto routeInternal{std::make_shared<RouteInternal>(
+        RouteInternal{route.id, route.name, lineInternal, std::move(stops)})};
+
+    for (size_t idx{0}; idx < routeInternal->stops.size() - 1; ++idx)
+    {
+        const auto& thisStop{routeInternal->stops[idx]};
+        const auto& nextStop{routeInternal->stops[idx + 1]};
+        GraphEdge edge{routeInternal, nextStop, 0};
+
+        thisStop->edges.emplace_back(std::move(edge));
+    }
+
+    lineInternal->routes[route.id] = std::move(routeInternal);
+
+    return true;
 }
 
 } // namespace NetworkMonitor
